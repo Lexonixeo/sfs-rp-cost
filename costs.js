e00566af458baadc6b8230784a16034dc7d37422
Lexonix <alexnovi2018@list.ru>
2022-04-10 09:04:04 +0300

    Create costs.js

---
+++ costs.js
@@ -0,0 +1,154 @@
+var costs = [
+    {
+      "n": "Capsule",
+      "cost": 50000
+    },
+    {
+      "n": "CapsuleNew",
+      "cost": 60000
+    },
+    {
+      "n": "Parachute",
+      "cost": 5000
+    },
+    {
+      "n": "Side Separator",
+      "cost": 5000
+    },
+    {
+      "n": "Separator",
+      "cost": 2000,
+      "width": true
+    },
+    {
+      "n": "Fuel Tank",
+      "cost": 4750,
+      "areas": true,
+      "fuel": true,
+      "fuelCost": 1475
+    },
+    {
+      "n": "Engine Hawk",
+      "cost": 50000,
+      "esize": true
+    },
+    {
+      "n": "Engine Valiant",
+      "cost": 40000,
+      "esize": true
+    },
+    {
+      "n": "Engine Kolibri",
+      "cost": 30000,
+      "esize": true
+    },
+    {
+      "n": "Cone",
+      "cost": 200,
+      "size": true
+    },
+    {
+      "n": "Cone Side",
+      "cost": 200,
+      "size": true
+    },
+    {
+      "n": "Parachute Side",
+      "cost": 5000
+    },
+    {
+      "n": "Fairing Cone",
+      "cost": 500,
+      "areat": true
+    },
+    {
+      "n": "Fairing",
+      "cost": 500,
+      "areas": true
+    },
+    {
+      "n": "Side Fuselage",
+      "cost": 150,
+      "size": true
+    },
+    {
+      "n": "Engine Frontier",
+      "cost": 80000,
+      "esize": true
+    },
+    {
+      "n": "Engine Titan",
+      "cost": 100000,
+      "esize": true
+    },
+    {
+      "n": "Placeholder ION",
+      "cost": 50000
+    },
+    {
+      "n": "Probe",
+      "cost": 15000
+    },
+    {
+      "n": "Solar Array 2",
+      "cost": 10000
+    },
+    {
+      "n": "Solar Array 3",
+      "cost": 20000
+    },
+    {
+      "n": "RCS",
+      "cost": 3000
+    },
+    {
+      "n": "Strut",
+      "cost": 500,
+      "size": true
+    },
+    {
+      "n": "Docking Port",
+      "cost": 2000,
+      "width": true
+    },
+    {
+      "n": "Wheel Big",
+      "cost": 2000
+    },
+    {
+      "n": "Wheel Medium",
+      "cost": 1000
+    },
+    {
+      "n": "Landing Leg",
+      "cost": 5000
+    },
+    {
+      "n": "Landing Leg Big",
+      "cost": 8000
+    },
+	{
+      "n": "Heat Shield",
+      "cost": 5000,
+	  "width": true
+    },
+	{
+      "n": "Heat Shield Hollow",
+      "cost": 5000,
+	  "width": true
+    },
+	{
+      "n": "Base Small",
+      "cost": 5000,
+	  "areat": true,
+	  "fuelt": true,
+	  "fuelCost": 1475
+    },
+	{
+      "n": "Base",
+      "cost": 5000,
+	  "areat": true,
+	  "fuelt": true,
+	  "fuelCost": 1475
+    }
+  ]
